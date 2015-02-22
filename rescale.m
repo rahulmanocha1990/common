cb30760e75c3b0681cc548a80e2828a5e363a1e5
rahulmanocha1990 <rahulmanocha1990@tamu.edu>
2015-02-22 13:10:25 -0600

    rescale edited

--- rescale.m
+++ rescale.m
@@ -1,8 +1,8 @@
-function Y = rescale(X,new_max,new_min,method)
+function Y = rescale(X,new_max,new_min)
 
-if(method='minmax')
+%if(method='minmax')
 old_max=max(X);
 old_min=min(X);
 Y=((new_max-new_min)/(old_max-old_min))*(X-old_min) + new_min;
-elseif(method=='mean')
+%elseif(method=='mean')
 
